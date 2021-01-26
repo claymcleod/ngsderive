b54fc5463d8d8109d3f66b32e2b0bc004e87af90
Andrew Frantz <andrew.frantz@stjude.org>
2021-01-26 14:36:32 -0500

    style(encoding.py): format with black

--- ngsderive/commands/encoding.py
+++ ngsderive/commands/encoding.py
@@ -15,21 +15,19 @@ SANGER_SET = set([i for i in range(33, 127)])
 ILLUMINA_1_0_SET = set([i for i in range(59, 127)])
 ILLUMINA_1_3_SET = set([i for i in range(64, 127)])
 
-def main(ngsfiles,
-         outfile=sys.stdout,
-         delimiter="\t",
-         n_samples=1000000):
-    
-    writer = csv.DictWriter(outfile,
-                            fieldnames=[
-                                "File", "Evidence", "ProbableEncoding"
-                            ],
-                            delimiter=delimiter)
+
+def main(ngsfiles, outfile=sys.stdout, delimiter="\t", n_samples=1000000):
+
+    writer = csv.DictWriter(
+        outfile,
+        fieldnames=["File", "Evidence", "ProbableEncoding"],
+        delimiter=delimiter,
+    )
     writer.writeheader()
     outfile.flush()
 
     if n_samples < 1:
-      n_samples = None
+        n_samples = None
 
     for ngsfilepath in ngsfiles:
         read_lengths = defaultdict(int)
@@ -39,7 +37,7 @@ def main(ngsfiles,
             result = {
                 "File": ngsfilepath,
                 "Evidence": "File not found.",
-                "ProbableEncoding": "N/A"
+                "ProbableEncoding": "N/A",
             }
             writer.writerow(result)
             outfile.flush()
@@ -47,40 +45,50 @@ def main(ngsfiles,
 
         if ngsfile.filetype != NGSFileType.FASTQ:
             raise RuntimeError(
-                "Invalid file: {}. `encoding` currently only supports FASTQ files!"
-                .format(ngsfilepath))
+                "Invalid file: {}. `encoding` currently only supports FASTQ files!".format(
+                    ngsfilepath
+                )
+            )
 
         score_set = set()
         for read in itertools.islice(ngsfile, n_samples):
-            for char in read['quality']:
+            for char in read["quality"]:
                 score_set.add(ord(char))
-        
+
         max_phred_score = chr(max(score_set))
         min_phred_score = chr(min(score_set))
         if score_set <= ILLUMINA_1_3_SET:
             result = {
                 "File": ngsfilepath,
-                "Evidence": "Phred range: {}-{}".format(min_phred_score, max_phred_score),
-                "ProbableEncoding": "Illumina 1.3"
+                "Evidence": "Phred range: {}-{}".format(
+                    min_phred_score, max_phred_score
+                ),
+                "ProbableEncoding": "Illumina 1.3",
             }
         elif score_set <= ILLUMINA_1_0_SET:
             result = {
                 "File": ngsfilepath,
-                "Evidence": "Phred range: {}-{}".format(min_phred_score, max_phred_score),
-                "ProbableEncoding": "Solexa/Illumina 1.0"
+                "Evidence": "Phred range: {}-{}".format(
+                    min_phred_score, max_phred_score
+                ),
+                "ProbableEncoding": "Solexa/Illumina 1.0",
             }
         elif score_set <= SANGER_SET:
             result = {
                 "File": ngsfilepath,
-                "Evidence": "Phred range: {}-{}".format(min_phred_score, max_phred_score),
-                "ProbableEncoding": "Sanger/Illumina 1.8"
+                "Evidence": "Phred range: {}-{}".format(
+                    min_phred_score, max_phred_score
+                ),
+                "ProbableEncoding": "Sanger/Illumina 1.8",
             }
         else:
             result = {
                 "File": ngsfilepath,
-                "Evidence": "Phred values outside known encoding ranges: {}-{}".format(min_phred_score, max_phred_score),
-                "ProbableEncoding": "Unknown"
+                "Evidence": "Phred values outside known encoding ranges: {}-{}".format(
+                    min_phred_score, max_phred_score
+                ),
+                "ProbableEncoding": "Unknown",
             }
-        
+
         writer.writerow(result)
         outfile.flush()
